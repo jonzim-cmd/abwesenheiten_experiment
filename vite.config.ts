de31a2f1123c0a2864829811a974547985ad9e08
jonzim-cmd <jonzim@posteo.de>
2025-01-16 21:35:34 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
-  base: '/abwesenheiten/',
   plugins: [react(), runtimeErrorOverlay(), themePlugin()],
   resolve: {
     alias: {
